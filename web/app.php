65a5d128493cde58a38747aa7465895c3eabe9da
Igor Botnaru <provodnick@gmail.com>
2018-02-08 16:31:52 +0200

    q

--- web/app.php
+++ web/app.php
@@ -4,15 +4,18 @@ if ($_SERVER['REQUEST_URI'] === '/admin/users') {
     require dirname(__DIR__) . '/src/users.php';
 }
 
-if ($_SERVER['REQUEST_URI'] === '/admin/users/upload') {
-    require dirname(__DIR__) . '/src/users.php';
-}
-if ($_SERVER['REQUEST_URI'] === '/admin/users/add') {
-    require dirname(__DIR__) . '/src/users.php';
-}
-if ($_SERVER['REQUEST_URI'] === '/admin/users') {
-    require dirname(__DIR__) . '/src/users.php';
-}
-if ($_SERVER['REQUEST_URI'] === '/admin/users') {
-    require dirname(__DIR__) . '/src/users.php';
-}
+//if ($_SERVER['REQUEST_URI'] === '/admin/users/upload') {
+//    require dirname(__DIR__) . '/src/users.php';
+//}
+//
+//if ($_SERVER['REQUEST_URI'] === '/admin/users/add') {
+//    require dirname(__DIR__) . '/src/users.php';
+//}
+//
+//if ($_SERVER['REQUEST_URI'] === '/admin/users') {
+//    require dirname(__DIR__) . '/src/users.php';
+//}
+//
+//if ($_SERVER['REQUEST_URI'] === '/admin/users') {
+//    require dirname(__DIR__) . '/src/users.php';
+//}
