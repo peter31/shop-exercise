04ecc1e8839b0b64b0eec08edf2cb4c1199d34b3
Igor Botnaru <provodnick@gmail.com>
2018-02-08 16:28:52 +0200

    q

--- web/app.php
+++ web/app.php
@@ -3,3 +3,16 @@
 if ($_SERVER['REQUEST_URI'] === '/admin/users') {
     require dirname(__DIR__) . '/src/users.php';
 }
+
+if ($_SERVER['REQUEST_URI'] === '/admin/users/upload') {
+    require dirname(__DIR__) . '/src/users.php';
+}
+if ($_SERVER['REQUEST_URI'] === '/admin/users/add') {
+    require dirname(__DIR__) . '/src/users.php';
+}
+if ($_SERVER['REQUEST_URI'] === '/admin/users') {
+    require dirname(__DIR__) . '/src/users.php';
+}
+if ($_SERVER['REQUEST_URI'] === '/admin/users') {
+    require dirname(__DIR__) . '/src/users.php';
+}
