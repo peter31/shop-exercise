ed84c00f0b97247a9a50e2f2d34f48520dab1a59
Igor Botnaru <provodnick@gmail.com>
2018-04-02 11:22:02 +0300

    scripts updated

--- src/Advert/Resources/templates/list.php
+++ src/Advert/Resources/templates/list.php
@@ -1,12 +1,15 @@
 <?php include dirname(__DIR__, 3) . '/Common/Resources/templates/header.php' ?>
-    <div class="adverts">
-        <table>
-<?php foreach ($adverts as $advert) { ?>
+<div class="adverts">
+    <table>
+        <?php foreach ($adverts as $advert) { ?>
             <tr>
                 <td class="title"><a href="/adverts/item?id=<?php echo $advert['id'] ?>"><?php echo $advert['title'] ?></a></td>
                 <td class="created"><?php echo $advert['created'] ?></td>
             </tr>
-<?php } ?>
-        </table>
-    </div>
+        <?php } ?>
+    </table>
+</div>
 <?php include dirname(__DIR__, 3) . '/Common/Resources/templates/footer.php' ?>
+
+
+
--- src/Common/Resources/templates/header.php
+++ src/Common/Resources/templates/header.php
@@ -4,59 +4,14 @@
     <meta charset="UTF-8">
     <title><?php echo $title ?></php></title>
     <link rel="stylesheet" type="text/css" href="normalize.css">
-    <style type="text/css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <style>
 
-        body {
-            width: 1000px;
-            margin: 0 auto;
-        }
-
-        .header {
-            background-color: cornflowerblue;
-            width: auto ;
-            margin: 0 auto;
-            padding: 8px;
-        }
-
-        .logo {
-            background-color: black;
-            color: #dddddd;
-            width: 50px;
-            text-align: center;
-            padding: 8px;
-        }
-
-        .adverts {
-            width: inherit;
-            margin: 0 auto;
-        }
-        table {
-            width: inherit;
-            font-family: arial, sans-serif;
-            font-size: small;
-            border-collapse: collapse;
-        }
-
-        .title {
-            width: 75%;
-            border: 1px solid #dddddd;
-            text-align: left;
-            padding: 8px;
-        }
-
-        .created {
-            width: 25%;
-            border: 1px solid #dddddd;
-            text-align: center;
-            padding: 8px;
-        }
-
-        .footer {
-            text-align: center;
-        }
     </style>
 </head>
 <body>
     <div class="header">
+        <div class="logo">LOGO</div>
+        <div class="logo">LOGO</div>
         <div class="logo">LOGO</div>
     </div>
