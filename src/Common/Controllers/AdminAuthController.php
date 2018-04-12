d55c870c7c4cfb76f2f239253696fde8346d2d37
Igor Botnaru <provodnick@gmail.com>
2018-04-12 18:50:04 +0300

    scripts updated

--- src/Common/Controllers/AdminAuthController.php
+++ src/Common/Controllers/AdminAuthController.php
@@ -14,20 +14,19 @@ class AdminAuthController
         require dirname(__DIR__, 3) . '/config.php';
 
         if ($_POST['user'] === $admin['user'] && $_POST['pass'] === $admin['pass']) {
-            $_SESSION['admin_auth_login'] = '';
+            $_SESSION['admin_auth_login'] = 'OK';
             if (array_key_exists('admin_back_url', $_SESSION)) {
-                header('Location: . "$_SESSION[\'admin_back_url\']" . ');
+                header('Location: ' . $_SESSION['admin_back_url']);
             } else {
                 header('Location: /admin/adverts');
             }
         }
-
         header('Location: /admin/login');
     }
 
     public function logOutAction()
     {
-        unset($_SESSION);
+        $_SESSION = array();
         header('Location: /admin/login');
     }
 }
