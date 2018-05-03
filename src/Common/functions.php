76c1bbf24896a34de27ff3e62fe5407b32134fb5
Peter Zaharia <peton3@gmail.com>
2018-05-03 18:03:42 +0300

    Added symfony validator

--- src/Common/functions.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function autoload($className)
-{
-    $classArr = explode('\\', $className);
-    $path = dirname(__DIR__, 2) . '/src/' . implode('/', $classArr) . '.php';
-    require_once $path;
-}
