40bb700f8f9b926164145095fe1fcfda3b575d11
Peter Zaharia <peton3@gmail.com>
2018-06-10 18:34:11 +0300

    Update docker container config

--- init.sh
+++ init.sh
@@ -4,4 +4,4 @@ docker exec exercise composer install;
 
 docker exec exercise cp config.php.dist config.php;
 
-mysql -h0.0.0.0 -P3308 -uexercise -pqwerty < ./scheme.sql;
+mysql exercise -h0.0.0.0 -P3308 -uexercise -pqwerty < ./scheme.sql;
